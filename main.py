c27a906450960b39086b09584becef8f3b58267f
JJAsus <jjairoph@gmail.com>
2016-06-27 21:13:10 -0500

    Solo los medicamentos activos + nuevo azul

--- main.py
+++ main.py
@@ -25,9 +25,9 @@ import logging
 
 #pdb.set_trace()
 
-CLOUDSQL_PROJECT = 'farmalium'
-CLOUDSQL_INSTANCE = 'farmalium_latin1'
-LOCALSQL_INSTANCE = 'farmalium_latin1'
+CLOUDSQL_PROJECT = 'pharmallium'
+CLOUDSQL_INSTANCE = 'pharmallium'
+LOCALSQL_INSTANCE = 'pharmallium'
 
 #PASSWD_LOCAL = "farmalium2016"
 PASSWD_LOCAL = "elpdhsqep"
@@ -42,12 +42,12 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 def handle_404(request, response, exception):
     direccion = request.path + request.url
     template_values = {
-        'titulo': 'Pagina no encontrada!!!',
-        'mensaje': direccion,
-        'mensaje1': '404 pagina no existe',
+        'mensaje0': 'Pagina no encontrada!!!',
+        'mensaje1': 'Podrias volver ',
+        'mensaje2': 'pero nosotros te invitamos a seguir siempre',
     }
     logging.exception(exception)
-    template = JINJA_ENVIRONMENT.get_template('/templates/warning.html')
+    template = JINJA_ENVIRONMENT.get_template('/templates/error.html')
     response.write(template.render(template_values))
     response.set_status(404)
 
@@ -55,8 +55,18 @@ def handle_500(request, response, exception):
     direccion = request.path
     url = request.url
     logging.exception(exception)
-    response.write('A server error occurred!jjjj' + direccion + url)
+    mensage = exception
+    template_values = {
+        'mensaje0': 'Error en el servidor!!!',
+        'mensaje1': str(mensage),
+        'mensaje2': 'Error interno',
+    }
+    logging.exception(exception)
+    template = JINJA_ENVIRONMENT.get_template('/templates/error.html')
+    response.write(template.render(template_values))
     response.set_status(500)
+
+
 
 
 class Resultado(webapp2.RequestHandler):
@@ -106,19 +116,19 @@ class MainPage(webapp2.RequestHandler):
 
         # When running on Google App Engine, use the special unix socket
         # to connect to Cloud SQL.
-        if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
-            db = MySQLdb.connect(
-                unix_socket='/cloudsql/farmalium:us-central1:farmalium'.format(
-                    CLOUDSQL_PROJECT,
-                    CLOUDSQL_INSTANCE),
-                user='root', passwd="elpdhsqep", db=CLOUDSQL_INSTANCE)
-        # When running locally, you can either connect to a local running
-        # MySQL instance, or connect to your Cloud SQL instance over TCP.
-        else:
-            db = MySQLdb.connect(host='localhost', user='root', passwd=PASSWD_LOCAL, db=LOCALSQL_INSTANCE )
-
-        cursor = db.cursor()
-        cursor.execute('select distinct producto from invimacompletaexcel order by 1')
+        # if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
+        #     db = MySQLdb.connect(
+        #         unix_socket='/cloudsql/pharmallium:us-central1:pharmallium'.format(
+        #             CLOUDSQL_PROJECT,
+        #             CLOUDSQL_INSTANCE),
+        #         user='conexion', passwd="elpdhsqep", db=CLOUDSQL_INSTANCE)
+        # # When running locally, you can either connect to a local running
+        # # MySQL instance, or connect to your Cloud SQL instance over TCP.
+        # else:
+        #     db = MySQLdb.connect(host='localhost', user='root', passwd=PASSWD_LOCAL, db=LOCALSQL_INSTANCE )
+        #
+        # cursor = db.cursor()
+        # cursor.execute('select distinct producto_nuevo from pharmallium.invima_depu order by 1')
 
         #Muestra el resultado de la CONSULTA
         #for r in cursor.fetchall():
@@ -162,10 +172,10 @@ class Consulta(webapp2.RequestHandler):
 
             if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
                     db = MySQLdb.connect(
-                        unix_socket='/cloudsql/farmalium:us-central1:farmalium'.format(
+                        unix_socket='/cloudsql/pharmallium:us-central1:db'.format(
                         CLOUDSQL_PROJECT,
                         CLOUDSQL_INSTANCE),
-                    user='root', passwd="elpdhsqep", db=CLOUDSQL_INSTANCE)
+                    user='root', passwd="chelin1974", db=CLOUDSQL_INSTANCE)
             # When running locally, you can either connect to a local running
             # MySQL instance, or connect to your Cloud SQL instance over TCP.
             else:
@@ -184,10 +194,13 @@ class Consulta(webapp2.RequestHandler):
                     filtro = filtro + parametros
 
             cursor = db.cursor()
-            query = 'Select distinct a.producto, a.descripcion_atc From invimacompletaexcel as a inner join ' \
-                    '(SELECT distinct descripcion_atc FROM invimacompletaexcel where producto like %s ) as b on a.descripcion_atc = b.descripcion_atc' \
-                    ' and a.estado_cum = %s and muestra_medica = %s and producto like %s order by 1'
-            cursor.execute(query, (medicamento, estadoActivo, muestraMedica, filtro,))
+            query = 'Select distinct a.producto_nuevo, a.descripcion_atc_unido From pharmallium.invima_depu as a inner join ' \
+                    '(SELECT distinct descripcion_atc_unido FROM pharmallium.invima_depu where producto_nuevo like %s ) as b on a.descripcion_atc_unido = b.descripcion_atc_unido' \
+                    ' and producto_nuevo like %s order by 1'
+                    #' and a.estado_cum = %s and muestra_medica = %s ' \
+
+            #cursor.execute(query, (medicamento, estadoActivo, muestraMedica, filtro,))
+            cursor.execute(query, (medicamento, filtro,))
             my_list = []
             #for r in cursor.fetchmany(200):
             my_list = cursor.fetchmany(200)
